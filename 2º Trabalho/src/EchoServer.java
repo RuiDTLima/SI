ca430739c05dfbdbfae7881a69de2c4ac079aa6a
Luanab <luana.b.silva@hotmail.com>
2017-11-20 14:07:32 +0000

    Exercises 1 through 5

--- 2º Trabalho/src/EchoServer.java
+++ 2º Trabalho/src/EchoServer.java
@@ -1,38 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import javax.net.ssl.*;
+import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 public class EchoServer {
 
     private static final int LISTEN_PORT = 8080;
 
     public static void main(String[] args) {
-
-        if (args.length != 1) {
-            System.err.println("Usage: java EchoServer <port number>");
-            System.exit(1);
-        }
-
         String portDef = System.getenv("PORT");
         int port = portDef != null ? Integer.valueOf(portDef) : LISTEN_PORT;
-
-        try (
-                ServerSocket serverSocket = new ServerSocket(port);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-        ) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
-            }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + port + " or listening for a connection");
-            System.out.println(e.getMessage());
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(Factory.getKeyManagers("resources/localhost.pfx"), Factory.getTrustManagers("resources/CA1.jks"), null);
+            do {
+                try (
+                        SSLServerSocket serverSocket = (SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(port);
+                        SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    clientSocket.setWantClientAuth(true);
+                    char[] buffer = new char[1024];
+                    while (in.read(buffer) > 0) {
+                        String body = new String(buffer).trim();
+                        out.println("HTTP/1.1 200 OK");
+                        out.println("Content-type: text/plain");
+                        out.println("Content-length: " + body.length() * 2);
+                        out.println();
+                        out.println(body);
+                        out.println();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Exception caught when trying to listen on port " + port + " or listening for a connection");
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            } while (true);
+        } catch (NoSuchAlgorithmException | CertificateException | UnrecoverableKeyException | KeyManagementException | KeyStoreException | IOException e) {
+            e.printStackTrace();
         }
     }
 }
